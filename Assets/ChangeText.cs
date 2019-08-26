4eaab419841e4c51078b9dfc0ff207479387661d
MBurak OLGUN <mburakolgun@gmail.com>
2019-08-26 23:39:49 +0300

    Buton On Click
    C# eğitiminde buton on clik olayının çalışma prensibini anlatan basit bir kod parçacığı

---
+++ Assets/ChangeText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeText : MonoBehaviour
+{
+    public UnityEngine.UI.Text textyazi;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeIt()
+    {
+        textyazi.text = "HOW ARE U?";
+
+    }
+}
